29bbbbf3c6629a190d4d31ebe5d567910cb9dbaf
Gregor Riegler <gregor.riegler@ebcont.com>
2022-08-22 00:07:43 +0200

    Participants can score

--- src/model/Participant.spec.ts
+++ src/model/Participant.spec.ts
@@ -1,10 +1,95 @@
 import { Participant } from "./Participant";
 
 describe('Participant', () => {
-    it('has a name', () => {
-        const participant = new Participant('Gregor');
+
+    let participant;
+
+    beforeEach(() => {
+        participant = new Participant('Gregor');
+    })
+
+
+    it('starts with no badges and no points', () => {
         expect(participant.name()).toBe('Gregor');
+        expect(participant.badges()).toEqual([]);
+        expect(participant.pointsFor('Driver')).toEqual(0);
+        expect(participant.pointsFor('Navigator')).toEqual(0);
+        expect(participant.pointsFor('Mobber')).toEqual(0);
     });
-    
-    
+
+    it('scores as Driver', () => {
+        participant.score('Driver');
+
+        expect(participant.badges()).toEqual([]);
+        expect(participant.pointsFor('Driver')).toEqual(1);
+        expect(participant.pointsFor('Navigator')).toEqual(0);
+        expect(participant.pointsFor('Mobber')).toEqual(0);
+    });
+
+    it('scores two times as Navigator', () => {
+        participant.scoreTimes('Navigator', 2);
+
+        expect(participant.badges()).toEqual([]);
+        expect(participant.pointsFor('Driver')).toEqual(0);
+        expect(participant.pointsFor('Navigator')).toEqual(2);
+        expect(participant.pointsFor('Mobber')).toEqual(0);
+    });
+
+    it('earns Mobber Badge', () => {
+        participant.scoreTimes('Mobber', 3);
+
+        expect(participant.badges()).toEqual(['Mobber']);
+        expect(participant.pointsFor('Driver')).toEqual(0);
+        expect(participant.pointsFor('Navigator')).toEqual(0);
+        expect(participant.pointsFor('Mobber')).toEqual(3);
+    });
+
+    it('earns all Badges', () => {
+        participant.scoreTimes('Mobber', 3);
+        participant.scoreTimes('Driver', 3);
+        participant.scoreTimes('Navigator', 3);
+
+        expect(participant.badges()).toEqual(expect.arrayContaining(['Mobber', 'Driver', 'Navigator']));
+        expect(participant.pointsFor('Driver')).toEqual(3);
+        expect(participant.pointsFor('Navigator')).toEqual(3);
+        expect(participant.pointsFor('Mobber')).toEqual(3);
+    });
+
+
+    it('cannot score for a Role that is not yet selected', () => {
+        participant.score('Researcher');
+
+        expect(participant.badges()).toEqual([]);
+        expect(participant.pointsFor('Driver')).toEqual(0);
+        expect(participant.pointsFor('Navigator')).toEqual(0);
+        expect(participant.pointsFor('Mobber')).toEqual(0);
+        expect(participant.pointsFor('Researcher')).toEqual(0);
+    });
+
+    it('can select level 2 Role after gaining a level 1 Badge', () => {
+        participant.scoreTimes('Mobber', 3);
+
+        participant.selectRole('Researcher');
+        participant.score('Researcher');
+
+        expect(participant.pointsFor('Researcher')).toEqual(1);
+    });
+
+    it('does not reset points on selecting an already selected role', () => {
+        participant.score('Mobber');
+
+        participant.selectRole('Mobber');
+
+        expect(participant.pointsFor('Mobber')).toEqual(1);
+    });
+
+
+    it('cannot select a level 2 role without gaining a level 1 Badge', () => {
+        const act = () => participant.selectRole('Researcher');
+
+        expect(act).toThrow();
+        participant.score('Researcher');
+        expect(participant.pointsFor('Researcher')).toEqual(0);
+    });
+
 })
--- src/model/Participant.ts
+++ src/model/Participant.ts
@@ -1,11 +1,59 @@
 export class Participant {
     private readonly _name: string;
 
+    private readonly _points = new Map([
+        ["Driver", 0],
+        ["Navigator", 0],
+        ["Mobber", 0],
+    ]);
+    private _badges = new Set<string>();
+
     constructor(name: string) {
         this._name = name;
     }
 
     name() {
         return this._name;
+    }
+
+    badges() {
+        return Array.from(this._badges);
+    }
+    
+    scoreTimes(role, times) {
+        for (let i = 0; i < times; i++) {
+            this.score(role);
+        }
+    }
+
+    score(role: string) {
+        if (!this._points.has(role)) {
+            return;
+        }
+        this.increasePointsFor(role);
+        if (this._points.get(role) >= 3) {
+            this._badges.add(role);
+        }
+    }
+
+    private increasePointsFor(role: string) {
+        this._points.set(role, this._points.get(role) + 1);
+    }
+
+    pointsFor(role: string) {
+        if (!this._points.has(role)) {
+            return 0;
+        }
+        return this._points.get(role);
+    }
+
+    selectRole(role: string) {
+        if(this._points.has(role)) {
+            return;
+        }
+        if(this._badges.size === 0) {
+            throw Error("Need a level 1 Badge first.");
+        }
+        this._points.set(role, 0);
     }
 }
