3ca6dc7628aa5348e25270affaea276c60c1ccec
Gregor Riegler <gregor.riegler@ebcont.com>
2022-08-25 20:14:35 +0200

    refactor

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,43 +1,49 @@
 import React, { useState } from "react";
 import { Participant } from "./model/Participant";
 
+function RolePoints(props) {
+    function addDriverPoints(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const amount = formData.get("amount") as String;
+        props.playerState.player.scoreTimes(props.role, amount);
+        // TODO participant immutable!?
+        // TODO save everything in local storage or its gone after refresh
+        props.setPlayerState({
+            player: Object.create(props.playerState.player),
+            addingPointsFor: props.playerState.addingPointsFor.filter(it => it !== props.role)
+        });
+    }
+    
+    return <>
+        {props.playerState.addingPointsFor.includes(props.role) &&
+          <form onSubmit={addDriverPoints}><label>Add Points<input type="text" name="amount" defaultValue="0"/></label>
+            <button type="submit">Add</button>
+          </form>
+        }
+        {props.playerState.player.hasBadge(props.role) && <span>{props.role} Badge</span>}
+        </>
+}
+
 const Player = (props) => {
     const [playerState, setPlayerState] = useState({
         player: new Participant(props.playerName),
         addingPointsFor: []
     })
 
-    function addDriverPoints(e) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const amount = formData.get("amount") as String;
-        playerState.player.scoreTimes("Driver", amount);
-        // TODO participant immutable!?
-        // TODO save everything in local storage or its gone after refresh
-        setPlayerState({
-            player: Object.create(playerState.player),
-            addingPointsFor: playerState.addingPointsFor.filter(it => it !== "Driver")
-        });
-    }
-
-    function showRolePointsForm(role: string){
-        return ()=>
+    function showRolePointsForm(role: string) {
+        return () =>
             setPlayerState({
                 ...playerState,
                 addingPointsFor: [...playerState.addingPointsFor, role]
             });
     }
-    
+
     return <li title={playerState.player.name()}>
         {props.playerName}<br/>
         <label>Driver<input disabled={true} value={playerState.player.pointsFor("Driver")}/></label>
         <button onClick={showRolePointsForm("Driver")}>Add Points</button>
-        {playerState.addingPointsFor.includes("Driver") &&
-          <form onSubmit={addDriverPoints}><label>Add Points<input type="text" name="amount" defaultValue="0"/></label>
-            <button type="submit">Add</button>
-          </form>
-        }
-        {playerState.player.hasBadge("Driver") && <span>Driver Badge</span>}
+        <RolePoints role="Driver" playerState={playerState} setPlayerState={setPlayerState}/>
     </li>
 }
 
