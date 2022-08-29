4236116fafb079a23ab7b23ddb4b62e62356c9db
Gregor Riegler <gregor.riegler@ebcont.com>
2022-08-29 22:00:34 +0200

    refactor game

--- src/model/Game.spec.ts
+++ src/model/Game.spec.ts
@@ -1,43 +1,4 @@
-enum RoleIndex {
-    Driver,
-    Navigator,
-    Next
-}
-
-class Game {
-    private _players = [];
-    private _rotations = 0;
-
-    players() {
-        return this._players;
-    }
-
-    setPlayers(players: string) {
-        this._players = players.split(',')
-            .map(player => player.trim())
-            .filter(it => it !== "");
-    }
-
-    driver() {
-        return this.roleOf(RoleIndex.Driver);
-    }
-
-    navigator() {
-        return this.roleOf(RoleIndex.Navigator);
-    }
-
-    next() {
-        return this.roleOf(RoleIndex.Next);
-    }
-
-    private roleOf(index: RoleIndex) {
-        return this._players[(index + this._rotations) % this._players.length];
-    }
-
-    rotate() {
-        this._rotations++;
-    }
-}
+import { Game } from "./Game";
 
 describe('Game', () => {
     it('starts with an empty list of players', () => {
