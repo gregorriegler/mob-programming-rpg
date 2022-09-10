218ec800f2cf8ac8d8e59cb17926a3a7291f2f67
Gregor Riegler <gregor.riegler@ebcont.com>
2022-09-10 22:36:53 +0200

    change wsserver to port 80

--- wsserver/main.ts
+++ wsserver/main.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from "ws";
 import { WSServer } from "./WSServer";
 
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({port: 80})
 new WSServer(wss)
 
 console.log("WS Server running");
