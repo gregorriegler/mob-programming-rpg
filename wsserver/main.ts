9198fd627c85d52e9c061b356dbc90d8d93dec53
Gregor Riegler <gregor.riegler@ebcont.com>
2022-09-10 22:42:25 +0200

    use herokus port

--- wsserver/main.ts
+++ wsserver/main.ts
@@ -1,7 +1,9 @@
 import { WebSocketServer } from "ws";
 import { WSServer } from "./WSServer";
 
-const wss = new WebSocketServer({port: 80})
+const wss = new WebSocketServer({
+    port: process.env.PORT ? parseInt(process.env.PORT) : 80
+})
 new WSServer(wss)
 
 console.log("WS Server running");
