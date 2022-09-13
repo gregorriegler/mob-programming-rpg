df37ec3a49d85ef7af4ef3002b8899f4b30f485e
Gregor Riegler <gregor.riegler@ebcont.com>
2022-09-13 22:19:55 +0200

    auth cert

--- wsserver/main.ts
+++ wsserver/main.ts
@@ -1,9 +1,29 @@
 import { WebSocketServer } from "ws";
 import { WSServer } from "./WSServer";
+import * as Https from "https";
+import * as fs from "fs";
+import * as Http from "http";
 
-const wss = new WebSocketServer({
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080
-})
-new WSServer(wss)
+// const httpsServer = Https.createServer({
+//     cert: fs.readFileSync('server.cert'),
+//     key: fs.readFileSync('server.key'),
+// })
+// const wss = new WebSocketServer({
+//     server: httpsServer,
+// })
+//
+// new WSServer(wss);
+//
+// httpsServer.listen(process.env.PORT ? parseInt(process.env.PORT) : 8080)
 
 console.log("WS Server running");
+
+// Authorize Certificate
+const httpServer = Http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end(`97E6C78FB6FA12A557FC46B04C221BE3AF3E0DAEDDE916032348D73C57CB2F0D
+comodoca.com
+50a437712d4f433
+`)
+})
+httpServer.listen(process.env.PORT ? parseInt(process.env.PORT) : 8080)
