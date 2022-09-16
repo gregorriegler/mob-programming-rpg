27f45c0c32d1588676faecdcfc98e400fc7386a6
Gregor Riegler <gregor.riegler@ebcont.com>
2022-09-16 23:26:03 +0200

    credits

--- webapp/src/MobProgrammingRPG.tsx
+++ webapp/src/MobProgrammingRPG.tsx
@@ -34,7 +34,12 @@ const MobProgrammingRPG = (
         Game.withPlayers(startingPlayers, rotateAfter, gameId)
     ), wsServer, wsReconnect);
 
-    const [uiState, setUiState] = useState({showSettings: false, showWhoIsNext: false, timeIsOver: false});
+    const [uiState, setUiState] = useState({
+        showSettings: false,
+        showWhoIsNext: false,
+        showAbout: false,
+        timeIsOver: false
+    });
 
     useEffect(() => {
         if (noGameIdInUrl()) {
@@ -77,6 +82,14 @@ const MobProgrammingRPG = (
         setUiState({...uiState, showWhoIsNext: !uiState.showWhoIsNext, timeIsOver: false});
     }
 
+    function toggleAbout() {
+        setUiState({...uiState, showAbout: !uiState.showAbout, timeIsOver: false});
+    }
+
+    function closeAbout() {
+        setUiState({...uiState, showAbout: false});
+    }
+
     function closeWhoIsNext() {
         setUiState({...uiState, showWhoIsNext: false});
     }
@@ -107,6 +120,7 @@ const MobProgrammingRPG = (
                 <button className="rpgui-button" onClick={toggleSettings}><p>Settings</p></button>
                 <button className="rpgui-button" onClick={toggleHelp}><p>Help</p></button>
                 <button className="rpgui-button" onClick={rotate}><p>Rotate</p></button>
+                <button className="rpgui-button" onClick={toggleAbout}><p>About</p></button>
                 <TimerDisplay
                     rotateAfter={game.timer()}
                     status={game.timerStatus()}
@@ -152,6 +166,37 @@ const MobProgrammingRPG = (
                   <button type="submit" className="rpgui-button"><p>Save</p></button>
                   <button className="rpgui-button" onClick={toggleSettings}><p>Close</p></button>
                 </form>
+              </div>
+            }
+            {uiState.showAbout &&
+              <div className="help-overlay rpgui-container framed-golden-2">
+                <h1>Mob Programming RPG</h1>
+                <p>
+                  The goal of this game is to teach Mob Programming.<br/>
+                  It's focus is on remote usage, but you may as well use it onsite.<br/>
+                  Just share the link to your game with the other players.<br/>
+                  Find the source code on <a href="https://github.com/gregorriegler/mob-programming-rpg">github</a>
+                </p>
+
+                <p>
+                  The <a href="https://github.com/willemlarsen/mobprogrammingrpg">original Idea and roles</a> have been
+                  developed by Willem Larsen.<br/>
+                  Some of it is taken as is, but some is changed...<br/>
+                </p>
+
+                <p>
+                  Credits for the retro rpg styling go to RonenNess for creating the <a
+                  href="https://github.com/RonenNess/RPGUI">RPGUI</a><br/>
+                  Thank you, Chris Hamons for the <a href="https://opengameart.org/content/dungeon-crawl-32x32-tiles">dungeon
+                  crawler icons</a><br/>
+                  And also CraftPix for the <a href="https://opengameart.org/content/48-pirate-stuff-icons">pirate stuff
+                  icons</a><br/>
+                  And for the beautiful <a href="https://www.flaticon.com/free-icon/pirate-ship_1907877">Pirate Ship</a><br/>
+                </p>
+                <button className="rpgui-button golden close-button" onClick={closeAbout}>
+
+                  <p>Close</p>
+                </button>
               </div>
             }
         </div>
