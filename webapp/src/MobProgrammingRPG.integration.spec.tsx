e6310bd973dd5e3a571f282bd49bc1704a1f187d
Gregor Riegler <rieglerg85@gmail.com>
2023-02-23 17:22:55 +0000

    t remove unused code from integrated test
    Co-Authored-By: Rea <reasu@protonmail.com>

--- webapp/src/MobProgrammingRPG.integration.spec.tsx
+++ webapp/src/MobProgrammingRPG.integration.spec.tsx
@@ -1,40 +1,8 @@
-import MobProgrammingRPG from "./MobProgrammingRPG";
-import { act, fireEvent, render, screen, within } from "@testing-library/react";
+import { act, fireEvent, render, within } from "@testing-library/react";
 import React from "react";
 import execa from "execa";
-
-import { ClockStub, MilliSeconds } from "./model/Clock";
-import { DEFAULT_TIMER, Game } from "./model/Game";
-import { DRIVER_THEN_NAVIGATOR, NAVIGATOR_THEN_DRIVER } from "./model/Roles";
-
-function getPlayerListItems() {
-    const playerList = screen.getByRole('list', { name: /Player List/ });
-    return within(playerList).queryAllByRole("listitem");
-}
-
-function getSettingsButton() {
-    return screen.getByRole('button', { name: 'Settings' });
-}
-
-
-function getPlayersTextarea() {
-    return screen.getByLabelText('Change Players');
-}
-
-function getTimerInput() {
-    return screen.getByLabelText(/Change Timer/);
-}
-
-function getRotateButton() {
-    return screen.getByRole('button', { name: 'Rotate' });
-}
-
-function changePlayers(players: string) {
-    fireEvent.click(getSettingsButton());
-    fireEvent.change(screen.getByLabelText('Change Players'), { target: { value: players } });
-    fireEvent.click(screen.getByText("Save"));
-    fireEvent.click(screen.getByText("Close"));
-}
+import MobProgrammingRPG from "./MobProgrammingRPG";
+import { Game } from "./model/Game";
 
 const randomPort = () => {
     return `${8000 + Math.floor(Math.random() * 100)}`;
@@ -73,39 +41,12 @@ describe('Mob Programming RPG', () => {
 
     }, 30000)
 
-    // todo this is not running. The servers should just die on their own.
     afterEach(() => {
         child?.all?.removeAllListeners("data");
         child?.kill();
         // this kills all the wsservers:
         // kill -9 $(ps aux | grep ts-node | grep -v grep | grep -Po '    \d{4}')
     })
-
-    xit('gamestate will be propagated to another instance of MobProgrammingRPG on an update', async () => {
-        const result = render(
-            <>
-                <div id="rpg1"><MobProgrammingRPG initGame={new Game({ id: "gameId", players: ["Gregor", "Peter", "Rita", "Ben"] })} /></div>
-                <div id="rpg2"><MobProgrammingRPG initGame={new Game({ id: "gameId" })} /></div>
-            </>
-        );
-
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        const rpg1 = document.getElementById('rpg1')!;
-        const rpg2 = document.getElementById('rpg2')!;
-        const playerList1 = within(rpg1).getByRole('list', { name: /Player List/ });
-        expect(playerList1.childElementCount).toEqual(5);
-
-        const playerList2 = within(rpg2).getByRole('list', { name: /Player List/ });
-        expect(playerList2.childElementCount).toEqual(1);
-
-        await act(async () => {
-            const timerButton = within(rpg1).getByRole("button", { name: /Start/i })
-            fireEvent.click(timerButton)
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-        })
-
-        expect(playerList2.childElementCount).toEqual(5);
-    });
 
     const itButNotOnOurCi = process.env.CI ? xit : it;
 
