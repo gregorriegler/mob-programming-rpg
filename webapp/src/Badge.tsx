929a1ca6784251c9edecf5e3a08a7a3bf7b59c2a
Gregor Riegler <rieglerg85@gmail.com>
2023-05-25 14:07:56 +0000

    r Badge component into separate file
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>

---
+++ webapp/src/Badge.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Role } from "./model/Roles";
+
+export function Badge({ role }: any) {
+  function badgeSource(role: Role) {
+    const images = {
+      Driver: "driver-badge.png",
+      Navigator: "navigator-badge.png",
+      Mobber: "mobber-badge.png",
+      Researcher: "researcher-badge.png",
+      Sponsor: "sponsor-badge.png",
+      "Rear Admiral": "rear-admiral-badge.png",
+      Automationist: "automationist-badge.png",
+      Nose: "nose-badge.png",
+      Archivist: "archivist-badge.png",
+      "Traffic Cop": "traffic-cop-badge.png",
+      Disciplinarian: "disciplinarian-badge.png",
+    };
+    return `${process.env.PUBLIC_URL}/img/icons/${images[role]}`;
+  }
+
+  return (
+    <div className="rpgui-icon magic-slot">
+      {role !== undefined && (
+        <img
+          src={badgeSource(role)}
+          alt={role + " Badge"}
+          aria-label={role + " Badge"} />
+      )}
+    </div>
+  );
+}
--- webapp/src/PlayerDisplay.tsx
+++ webapp/src/PlayerDisplay.tsx
@@ -3,6 +3,7 @@ import { Role } from "./model/Roles";
 import { noOp } from "./model/Func";
 import { Player } from "./model/Player";
 import { RolePoints } from "./RolePoints";
+import { Badge } from "./Badge";
 
 const PlayerDisplay = ({
   player,
@@ -67,37 +68,6 @@ const PlayerDisplay = ({
         </form>
       )}
     </li>
-  );
-};
-
-const Badge = ({ role }: any) => {
-  function badgeSource(role: Role) {
-    const images = {
-      Driver: "driver-badge.png",
-      Navigator: "navigator-badge.png",
-      Mobber: "mobber-badge.png",
-      Researcher: "researcher-badge.png",
-      Sponsor: "sponsor-badge.png",
-      "Rear Admiral": "rear-admiral-badge.png",
-      Automationist: "automationist-badge.png",
-      Nose: "nose-badge.png",
-      Archivist: "archivist-badge.png",
-      "Traffic Cop": "traffic-cop-badge.png",
-      Disciplinarian: "disciplinarian-badge.png",
-    };
-    return `${process.env.PUBLIC_URL}/img/icons/${images[role]}`;
-  }
-
-  return (
-    <div className="rpgui-icon magic-slot">
-      {role !== undefined && (
-        <img
-          src={badgeSource(role)}
-          alt={role + " Badge"}
-          aria-label={role + " Badge"}
-        />
-      )}
-    </div>
   );
 };
 
