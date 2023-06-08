45ae01107e6fa60987c1af315e0d482720854a24
Nitsan Avni <nitsanav@gmail.com>
2023-06-08 14:11:47 +0000

    r extract ProgressBar component
    Co-Authored-By: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>
    Co-Authored-By: Michael R. Wolf <MichaelRWolf@att.net>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>

--- webapp/src/RoleSheet.tsx
+++ webapp/src/RoleSheet.tsx
@@ -3,97 +3,112 @@ import { Role } from "./model/Roles";
 import { Player } from "./model/Player";
 
 type AppleSauce = {
-  player: Player;
-  role: Role;
-  setUiState: any;
-  uiState: any;
-  updateGame: any;
-  featureFlagShowSkillsPerRole?: boolean;
-}
+    player: Player;
+    role: Role;
+    setUiState: any;
+    uiState: any;
+    updateGame: any;
+    featureFlagShowSkillsPerRole?: boolean;
+};
 
 export function RoleSheet({
-  player, 
-  role, 
-  setUiState, 
-  uiState, 
-  updateGame, 
-  featureFlagShowSkillsPerRole = !!process.env.REACT_APP_FEATURE_FLAG_SHOW_SKILLS_PER_ROLE
+    player,
+    role,
+    setUiState,
+    uiState,
+    updateGame,
+    featureFlagShowSkillsPerRole = !!process.env
+        .REACT_APP_FEATURE_FLAG_SHOW_SKILLS_PER_ROLE,
 }: AppleSauce): JSX.Element {
-  function showRolePointsForm(role: string) {
-    return () => setUiState({
-      addingPointsFor: [...uiState.addingPointsFor, role],
-    });
-  }
+    function showRolePointsForm(role: string) {
+        return () =>
+            setUiState({
+                addingPointsFor: [...uiState.addingPointsFor, role],
+            });
+    }
 
-  function addDriverPoints(e) {
-    e.preventDefault();
-    const amount = Number(new FormData(e.target).get("amount") as String);
-    player.scoreTimes(role, amount);
-    setUiState({
-      addingPointsFor: uiState.addingPointsFor.filter((it: any) => it !== role),
-    });
-    updateGame();
-  }
-//TODO: Extract progress bar as a component 
-  return (
-    <div className="role">
-      <hr />
+    function addDriverPoints(e) {
+        e.preventDefault();
+        const amount = Number(new FormData(e.target).get("amount") as String);
+        player.scoreTimes(role, amount);
+        setUiState({
+            addingPointsFor: uiState.addingPointsFor.filter(
+                (it: any) => it !== role
+            ),
+        });
+        updateGame();
+    }
 
-      <label className="role-label">
-        {role}
-        <div
-          className="rpgui-progress"
-          data-rpguitype="progress"
-          data-value={0.1}
-        >
-          <div className="rpgui-progress-track">
-            <div
-              className="rpgui-progress-fill"
-              style={{
-                left: "0px",
-                width: "" + player.percentageFor(role) + "%",
-              }}
-            ></div>
-          </div>
-          <div className=" rpgui-progress-left-edge"></div>
-          <div className=" rpgui-progress-right-edge"></div>
-        </div>
-        <input
-          disabled={true}
-          style={{ display: "none" }}
-          className="role-points"
-          value={player.pointsFor(role)} />
-      </label>
-      <button
-        onClick={showRolePointsForm(role)}
-        className="rpgui-button add-points-button"
-        aria-label={"Add " + role + " Points"}
-      >
-        <p>Earn</p>
-      </button>
-      {uiState.addingPointsFor.includes(role) && (
-        <form className="add-points-form" onSubmit={addDriverPoints}>
-          <label>
-            Add Points
-            <input
-              className="add-points-input"
-              type="text"
-              name="amount"
-              defaultValue="0" />
-          </label>
-          <button className="rpgui-button" type="submit">
-            <p>Add</p>
-          </button>
-        </form>
-      )}
-      {/* TODO: Add list of activities for this role */}
+    return (
+        <div className="role">
+            <hr />
 
-      {featureFlagShowSkillsPerRole && "junk that should be skills instead"}
+            <label className="role-label">
+                {role}
+                <ProgressBar player={player} role={role} />
+                <input
+                    disabled={true}
+                    style={{ display: "none" }}
+                    className="role-points"
+                    value={player.pointsFor(role)}
+                />
+            </label>
+            <button
+                onClick={showRolePointsForm(role)}
+                className="rpgui-button add-points-button"
+                aria-label={"Add " + role + " Points"}
+            >
+                <p>Earn</p>
+            </button>
+            {uiState.addingPointsFor.includes(role) && (
+                <form className="add-points-form" onSubmit={addDriverPoints}>
+                    <label>
+                        Add Points
+                        <input
+                            className="add-points-input"
+                            type="text"
+                            name="amount"
+                            defaultValue="0"
+                        />
+                    </label>
+                    <button className="rpgui-button" type="submit">
+                        <p>Add</p>
+                    </button>
+                </form>
+            )}
+            {/* TODO: Add list of activities for this role */}
 
-      {/*
+            {featureFlagShowSkillsPerRole &&
+                "junk that should be skills instead"}
+
+            {/*
                  some_function({role}) returns string (or list of strings) to display here....
                  <p>Text goes here</p>
               */}
-    </div>
-  );
+        </div>
+    );
+}
+
+// TODO - change params to only include the progress-bar width / percentage
+// TODO - maybe player could hold the percentage
+function ProgressBar({ player, role }: { player: Player; role: string }) {
+    return (
+        <div
+            className="rpgui-progress"
+            data-rpguitype="progress"
+            data-value={0.1}
+        >
+            <div className="rpgui-progress-track">
+                <div
+                    className="rpgui-progress-fill"
+                    style={{
+                        left: "0px",
+                        width: "" + player.percentageFor(role) + "%",
+                    }}
+                ></div>
+            </div>
+            <div className=" rpgui-progress-left-edge"></div>
+            <div className=" rpgui-progress-right-edge"></div>
+        </div>
+    );
 }
