dc3f6f6f9ebbcaeb1a7ff8ffa23926b692d65685
Gregor Riegler <rieglerg85@gmail.com>
2023-08-17 14:55:15 +0000

    t driver sees all todos
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Michael R. Wolf <MichaelRWolf@att.net>

--- webapp/src/RoleSheet.spec.tsx
+++ webapp/src/RoleSheet.spec.tsx
@@ -2,14 +2,16 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { Player } from "./model/Player";
 import { RoleSheet } from "./RoleSheet";
+import { roles } from "./model/Roles";
 
-it('driver does show driver skills', () => {
-    render(<RoleSheet player={new Player("Roger")} role="Driver" scorePoints={()=>{}}/>);
-    expect(screen.getByText("Ask a clarifying question about what to type")).toBeInTheDocument()
-    expect(screen.getByText("Ignore a direct instruction from someone who isn't the Navigator")).toBeInTheDocument()
+const role = roles.Driver;
+
+it('driver does show all driver skills', () => {
+  render(<RoleSheet player={new Player("Roger")} role={role.name} scorePoints={() => { }} />);
+  role.todos.forEach(todo => expect(screen.getByText(todo)).toBeInTheDocument())
 })
 
 it('navigator does not show driver skills', () => {
-    render(<RoleSheet player={new Player("Roger")} role="Navigator" scorePoints={()=>{}}/>);
-    expect(screen.queryByText("Ask a clarifying question about what to type")).toBeNull();
+  render(<RoleSheet player={new Player("Roger")} role="Navigator" scorePoints={() => { }} />);
+  expect(screen.queryByText("Ask a clarifying question about what to type")).toBeNull();
 })
