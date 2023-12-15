2243b290ce2340f8372884846e73b78bbcb0f320
Michael R. Wolf <MichaelRWolf@att.net>
2023-12-14 22:38:30 -0500

    . r Remove dead code

--- src/session_notes_cleanup/session_notes_cleaner.py
+++ src/session_notes_cleanup/session_notes_cleaner.py
@@ -70,38 +70,6 @@ class SessionNotesCleaner:
             return None
 
 
-def fn_normalize_coauthor_heading(contents):
-    return re.sub(r'^#+\s*.*Co-Author.*', '## Co-Authors', contents, flags=re.IGNORECASE | re.MULTILINE)
-
-
-def method_name(filename, original_contents):
-    contents = original_contents
-    date_as_string = SessionNotesCleaner().get_date_from_filename(filename)
-    contents = fn_applesauce(contents, date_as_string)
-    if contents == original_contents:
-        print(f"No changes were needed for the file: {filename}")
-    else:
-        print(f"Changes were made to the file: {filename}")
-        new_filename = filename + ".new"
-        with open(new_filename, 'w') as new_file:
-            new_file.write(contents)
-        shutil.copystat(filename, new_filename)
-        original_backup_filename = filename + ".original"
-        os.rename(filename, original_backup_filename)
-        os.rename(new_filename, filename)
-        print(f"You can view changes by issuing this command: diff -u {original_backup_filename} {filename}")
-
-
-def fn_applesauce(contents, date_from_filename):
-    if not SessionNotesCleaner().contains_session_date(contents):
-        contents = f"# Session Date: {date_from_filename}\n" + contents
-    if SessionNotesCleaner().contains_active_coauthors(contents) and \
-            SessionNotesCleaner().contains_inactive_coauthors(contents):
-        contents = SessionNotesCleaner().delete_inactive_coauthors(contents)
-    contents = fn_normalize_coauthor_heading(contents)
-    return contents
-
-
 def main():
     cleaner = SessionNotesCleaner()
     for filename in sys.argv[1:]:
