5e0deb9f45b8ee6d04bbea9565520d12930c19fe
Michael R. Wolf <MichaelRWolf@att.net>
2023-12-14 22:19:25 -0500

    . r Rename all functions to fn_*.  Move cleanup_file() into class.

--- src/session_notes_cleanup/session_notes_cleaner.py
+++ src/session_notes_cleanup/session_notes_cleaner.py
@@ -21,7 +21,7 @@ class SessionNotesCleaner:
         return bool(re.search(r'^#+\s*Active Co-Authors', text, re.IGNORECASE | re.MULTILINE))
 
     def cleanup_contents(self, text, session_date):
-        if not contains_session_date(text):
+        if not fn_contains_session_date(text):
             text = f"# Session Date: {session_date}\n" + text
         if self.contains_active_coauthors(text) and self.contains_inactive_coauthors(text):
             text = self.delete_inactive_coauthors(text)
@@ -33,13 +33,35 @@ class SessionNotesCleaner:
                       text,
                       flags=re.IGNORECASE | re.MULTILINE)
 
+    def cleanup_file(self, filename):
+        original_contents = fn_slurp_file(filename)
+        if original_contents is None:
+            print(f"File not found: {filename}")
+            return
+
+        contents = original_contents
+        date_as_string = get_date_from_filename(filename)
+        contents = self.cleanup_contents(contents, date_as_string)
+        if contents == original_contents:
+            print(f"No changes were needed for the file: {filename}")
+        else:
+            print(f"Changes were made to the file: {filename}")
+            new_filename = filename + ".new"
+            with open(new_filename, 'w') as new_file:
+                new_file.write(contents)
+            shutil.copystat(filename, new_filename)
+            original_backup_filename = filename + ".original"
+            os.rename(filename, original_backup_filename)
+            os.rename(new_filename, filename)
+            print(f"You can view changes by issuing this command: diff -u {original_backup_filename} {filename}")
+
 
 def get_date_from_filename(filename):
     match = re.search(r'(\d{4}-\d{2}-\d{2})', filename, re.IGNORECASE)
     return match.group(1) if match else None
 
 
-def slurp_file(filename):
+def fn_slurp_file(filename):
     try:
         with open(filename, 'r') as file:
             return file.read()
@@ -47,38 +69,31 @@ def slurp_file(filename):
         return None
 
 
-def contains_inactive_coauthors(contents):
-    return bool(re.search(r'^#+\s*Inactive Co-Authors', contents, re.IGNORECASE | re.MULTILINE))
+# def fn_contains_inactive_coauthors(contents):
+#     return bool(re.search(r'^#+\s*Inactive Co-Authors', contents, re.IGNORECASE | re.MULTILINE))
+#
+#
+# def fn_contains_active_coauthors(contents):
+#     return bool(re.search(r'^#+\s*Active Co-Authors', contents, re.IGNORECASE | re.MULTILINE))
 
 
-def contains_active_coauthors(contents):
-    return bool(re.search(r'^#+\s*Active Co-Authors', contents, re.IGNORECASE | re.MULTILINE))
-
-
-def contains_session_date(contents):
+def fn_contains_session_date(contents):
     return bool(re.search(r'^#+\s*Session Date', contents, re.IGNORECASE | re.MULTILINE))
 
 
-def delete_inactive_coauthors(contents):
+def fn_delete_inactive_coauthors(contents):
     return re.sub(r'^#+\s*Inactive Co-Authors.*?(?=^#|\Z)', '', contents,
                   flags=re.IGNORECASE | re.MULTILINE | re.DOTALL)
 
 
-def normalize_coauthor_heading(contents):
+def fn_normalize_coauthor_heading(contents):
     return re.sub(r'^#+\s*.*Co-Author.*', '## Co-Authors', contents, flags=re.IGNORECASE | re.MULTILINE)
 
 
-def cleanup_file(filename):
-    original_contents = slurp_file(filename)
-    if original_contents is None:
-        print(f"File not found: {filename}")
-        return
-
+def method_name(filename, original_contents):
     contents = original_contents
     date_as_string = get_date_from_filename(filename)
-
-    contents = applesauce(contents, date_as_string)
-
+    contents = fn_applesauce(contents, date_as_string)
     if contents == original_contents:
         print(f"No changes were needed for the file: {filename}")
     else:
@@ -93,18 +108,19 @@ def cleanup_file(filename):
         print(f"You can view changes by issuing this command: diff -u {original_backup_filename} {filename}")
 
 
-def applesauce(contents, date_from_filename):
-    if not contains_session_date(contents):
+def fn_applesauce(contents, date_from_filename):
+    if not fn_contains_session_date(contents):
         contents = f"# Session Date: {date_from_filename}\n" + contents
-    if contains_active_coauthors(contents) and contains_inactive_coauthors(contents):
-        contents = delete_inactive_coauthors(contents)
-    contents = normalize_coauthor_heading(contents)
+    if fn_contains_active_coauthors(contents) and fn_contains_inactive_coauthors(contents):
+        contents = fn_delete_inactive_coauthors(contents)
+    contents = fn_normalize_coauthor_heading(contents)
     return contents
 
 
 def main():
+    cleaner = SessionNotesCleaner()
     for filename in sys.argv[1:]:
-        cleanup_file(filename)
+        cleaner.cleanup_file(filename)
 
 
 if __name__ == "__main__":
