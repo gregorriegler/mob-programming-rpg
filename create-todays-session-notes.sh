43fcdd5986f70d8a760efc8e5890253c569b43bb
Gregor Riegler <rieglerg85@gmail.com>
2024-01-04 16:21:50 +0000

    . e improve session-notes template
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- create-todays-session-notes.sh
+++ create-todays-session-notes.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENT_DATE=$(date +"%Y-%m-%d")
 
 # Define the template and new file paths using the script's directory
-TEMPLATE="$SCRIPT_DIR/session-notes/session-notes-YYYY-MM-DD.md"
+TEMPLATE="$SCRIPT_DIR/session-notes/session-notes-template.md"
 NEW_FILE="$SCRIPT_DIR/session-notes/session-notes-$CURRENT_DATE.md"
 
 # Check if the template exists
@@ -24,7 +24,8 @@ fi
 
 # Create a new file from the template
 echo "# Session Date: ${CURRENT_DATE}" > "$NEW_FILE"
-cat "$TEMPLATE" >> "$NEW_FILE"
+# don't duplicate the session date line
+cat "$TEMPLATE" | tail -n +2 >> "$NEW_FILE"
 
 # Feedback to the user
 if [ $? -eq 0 ]; then
